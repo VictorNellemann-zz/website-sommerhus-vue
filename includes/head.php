12ef1cacb4343521aacb397f84741f12b3367f69
Victor Hjem <vnellemann@gmail.com>
2018-10-21 15:56:31 +0200

    npm install Bulma

--- includes/head.php
+++ includes/head.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-
+    
     require_once './core/functions.php';
     $query = require './core/bootstrap.php';
     $userstr = ' (Guest)';
@@ -19,7 +19,9 @@
     <title>$appname$userstr</title>
     <meta charset='utf-8' />
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href='./styling/main.css' type='text/css' rel='stylesheet'>
     <link href='./styling/fonts.css' type='text/css' rel='stylesheet'>
     <link href='https://fonts.googleapis.com/css?family=Mukta:400,700|Noto+Serif:400,700' rel='stylesheet'>
+    <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
 </head>
